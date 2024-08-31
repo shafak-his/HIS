73d3cc2042fb3bbda4882585208e0f3ab0b74e34
abdak3 <abd.ak3@gmail.com>
2024-09-01 02:36:50 +0300

    dialysis forms

---
+++ emata_hms/models/dial_nephrology.py
@@ -0,0 +1,21 @@
+from odoo import _, api, fields, models, exceptions, tools
+
+
+class EmHmsDialNephrology(models.Model):
+    _name = 'em.hms.dial.nephrology'
+    _description = 'Nephrology Clinic'
+    _rec_name = 'patient_id'
+    _inherit = ['mail.thread', 'mail.activity.mixin']
+    
+    patient_id = fields.Many2one('res.partner', 'Patient Name', required=True, domain=[('is_patient','=',True)])
+    visit_datetime = fields.Datetime('Visit Date/Time', required=True, tracking=True)
+    medical_history = fields.Char('Medical History', tracking=True)
+    pressure = fields.Float('Pressure', tracking=True)
+    current_complaint = fields.Char('Current Complaint', tracking=True)
+    diagnosis_id = fields.Many2one('em.hms.icd10', string='Diagnosis', required=True, tracking=True)
+    notes = fields.Char('Notes', tracking=True)
+    doctor_id = fields.Many2one('hr.employee', string='Doctor', tracking=True)
+    medication_request_ids = fields.Many2many('product.template', 'dial_nephrology_product_medication_rel','clinic_visit_id', 'product_id', string='Medication Requests',domain="[('is_medication', '=', True)]")
+    analysis_request_ids = fields.Many2many('product.template', 'dial_nephrology_product_analysis_rel','clinic_visit_id', 'product_id', string='Analysis Requests',domain="[('is_medical_analysis', '=', True)]")
+    image_request_ids = fields.Many2many('product.template', 'dial_nephrology_product_image_rel', 'clinic_visit_id','product_id', string='Image Requests',domain="[('is_medical_imaging', '=', True)]")
+    company_id = fields.Many2one('res.company', 'Medical Center', default = lambda self: self.env.company)
