1a8563b17ba713bfe83c7c17bfbd37a54093d0a5
shafak-his <his@shafak.org>
2025-11-08 14:47:34 +0300

    somechange

--- emata_hms/models/sale_order.py
+++ emata_hms/models/sale_order.py
@@ -4,6 +4,7 @@ class SaleOrder(models.Model):
     _inherit = 'sale.order'
     
     general_visit_id = fields.Many2one('em.hms.general.clinic.visit', string='General Clinic Visit')
+    hospitalization_id = fields.Many2one('em.hms.rhs.hospitalization', string='hospitalization Visit')
     gynochological_visit_id = fields.Many2one('em.hms.rhs.gynochological.clinic.visit', string='Gynochological Visit')
     pnc_visit_id = fields.Many2one('em.hms.rhs.pnc.visit', string='PNC Visit')
     mh_gap_id = fields.Many2one('em.hms.mh.gap', string='MH Gap')
