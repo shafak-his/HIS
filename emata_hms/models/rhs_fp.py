03f5c31b6138a306571d2dde75650018a7bc5c98
shafak-his <his@shafak.org>
2025-11-14 20:40:14 +0300

    somechange

--- emata_hms/models/rhs_fp.py
+++ emata_hms/models/rhs_fp.py
@@ -89,6 +89,11 @@ class EmHmsRHSFP(models.Model):
     recommendations = fields.Char('Recommendations And Treatment', tracking=True)
     examiner_id = fields.Many2one('hr.employee', string='Name Of Examiner', tracking=True, required=True)
     next_visit_date = fields.Date('Next Visit Date', tracking=True, required=True)
+    
+    state = fields.Selection([
+        ('draft', 'Draft'),
+        ('done', 'Done'),
+    ], string='Status', required=True, default='draft')
     notes = fields.Char('Notes', tracking=True)
     
 
