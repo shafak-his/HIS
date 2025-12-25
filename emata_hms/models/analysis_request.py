87e2753c222ab17f234ddaa5c2cf92d4cf164639
shafak-his <his@shafak.org>
2025-12-25 22:49:29 +0300

    somechange

--- emata_hms/models/analysis_request.py
+++ emata_hms/models/analysis_request.py
@@ -68,7 +68,7 @@ class EmHmsAnalysisRequestLine(models.Model):
     rhs_surgery_id = fields.Many2one('em.hms.rhs.surgery', string='RHS Surgery')
 
     result = fields.Char('Result', tracking=True)
-    attach_file = fields.Binary('Attachment', tracking=True)
+    attach_file = fields.Binary('Attachment')
     attach_file_name = fields.Char('Attachment Name', tracking=True)
     
     product_template_id = fields.Many2one('product.template', string='Product', domain="[('is_medical_analysis', '=', True)]", required=True)
